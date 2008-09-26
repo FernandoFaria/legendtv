b145396a84a1c7d0a3a936eeb7729235e669f6b9
Guy.Paddock <Guy.Paddock@fc7fc1dd-9c2b-0410-9c68-c9cb4ac4db1e>
2008-09-26 18:58:21 +0000

    Added current high-fidelity models.

---
+++ SE444/High-fidelity Models/LegendTV/SetupWizard.Designer.cs
@@ -0,0 +1,364 @@
+namespace LegendTV
+{
+    partial class SetupWizard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SetupWizard));
+            this.nextBtn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1DisplayDevice = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabPage2CalibrateBrightness = new System.Windows.Forms.TabPage();
+            this.tabPage3CalibrateContrast = new System.Windows.Forms.TabPage();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.prevBtn = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.headingLabel = new System.Windows.Forms.Label();
+            this.tabPage4CalibrateGamma = new System.Windows.Forms.TabPage();
+            this.tabPage5TunerDetection = new System.Windows.Forms.TabPage();
+            this.tabPage6TunerSetup = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1DisplayDevice.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tabPage2CalibrateBrightness.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // nextBtn
+            // 
+            this.nextBtn.Location = new System.Drawing.Point(486, 280);
+            this.nextBtn.Margin = new System.Windows.Forms.Padding(3, 0, 0, 0);
+            this.nextBtn.Name = "nextBtn";
+            this.nextBtn.Size = new System.Drawing.Size(137, 37);
+            this.nextBtn.TabIndex = 9;
+            this.nextBtn.Text = "Next Step >";
+            this.nextBtn.UseVisualStyleBackColor = true;
+            this.nextBtn.Click += new System.EventHandler(this.nextBtn_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 280);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 37);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Exit System Setup";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Appearance = System.Windows.Forms.TabAppearance.FlatButtons;
+            this.tabControl1.Controls.Add(this.tabPage1DisplayDevice);
+            this.tabControl1.Controls.Add(this.tabPage2CalibrateBrightness);
+            this.tabControl1.Controls.Add(this.tabPage3CalibrateContrast);
+            this.tabControl1.Controls.Add(this.tabPage4CalibrateGamma);
+            this.tabControl1.Controls.Add(this.tabPage5TunerDetection);
+            this.tabControl1.Controls.Add(this.tabPage6TunerSetup);
+            this.tabControl1.ItemSize = new System.Drawing.Size(0, 1);
+            this.tabControl1.Location = new System.Drawing.Point(9, 52);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(614, 228);
+            this.tabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.tabControl1.TabIndex = 11;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
+            // 
+            // tabPage1DisplayDevice
+            // 
+            this.tabPage1DisplayDevice.Controls.Add(this.tableLayoutPanel2);
+            this.tabPage1DisplayDevice.Controls.Add(this.label3);
+            this.tabPage1DisplayDevice.Controls.Add(this.label2);
+            this.tabPage1DisplayDevice.Location = new System.Drawing.Point(4, 5);
+            this.tabPage1DisplayDevice.Name = "tabPage1DisplayDevice";
+            this.tabPage1DisplayDevice.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1DisplayDevice.Size = new System.Drawing.Size(606, 219);
+            this.tabPage1DisplayDevice.TabIndex = 0;
+            this.tabPage1DisplayDevice.Text = "Display Device";
+            this.tabPage1DisplayDevice.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.radioButton4, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.radioButton5, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.radioButton6, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.radioButton7, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.radioButton8, 0, 4);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(6, 60);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 5;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(353, 115);
+            this.tableLayoutPanel2.TabIndex = 10;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(3, 3);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(184, 17);
+            this.radioButton4.TabIndex = 0;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Standard-definition (SD) television";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(3, 49);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(132, 17);
+            this.radioButton5.TabIndex = 1;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "CRT Computer monitor";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(3, 72);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(131, 17);
+            this.radioButton6.TabIndex = 2;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "LCD Computer monitor";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(3, 26);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(164, 17);
+            this.radioButton7.TabIndex = 4;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "High-definition (HD) television";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(3, 95);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(67, 17);
+            this.radioButton8.TabIndex = 5;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "Projector";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 178);
+            this.label3.Margin = new System.Windows.Forms.Padding(0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(600, 38);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "If you use more than one display device, choose the one you will be using most of" +
+                "ten with LegendTV.";
+            // 
+            // label2
+            // 
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 3);
+            this.label2.Margin = new System.Windows.Forms.Padding(0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(596, 54);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Welcome to LegendTV! This wizard will help you setup your system.\r\n\r\nTo start wit" +
+                "h, what type of display device are you using?";
+            // 
+            // tabPage2CalibrateBrightness
+            // 
+            this.tabPage2CalibrateBrightness.Controls.Add(this.label1);
+            this.tabPage2CalibrateBrightness.Controls.Add(this.label4);
+            this.tabPage2CalibrateBrightness.Location = new System.Drawing.Point(4, 5);
+            this.tabPage2CalibrateBrightness.Name = "tabPage2CalibrateBrightness";
+            this.tabPage2CalibrateBrightness.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2CalibrateBrightness.Size = new System.Drawing.Size(606, 219);
+            this.tabPage2CalibrateBrightness.TabIndex = 1;
+            this.tabPage2CalibrateBrightness.Text = "Black Level";
+            this.tabPage2CalibrateBrightness.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3CalibrateContrast
+            // 
+            this.tabPage3CalibrateContrast.Location = new System.Drawing.Point(4, 5);
+            this.tabPage3CalibrateContrast.Name = "tabPage3CalibrateContrast";
+            this.tabPage3CalibrateContrast.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3CalibrateContrast.Size = new System.Drawing.Size(606, 219);
+            this.tabPage3CalibrateContrast.TabIndex = 2;
+            this.tabPage3CalibrateContrast.Text = "Contrast";
+            this.tabPage3CalibrateContrast.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(3, 72);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(131, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "LCD Computer monitor";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // prevBtn
+            // 
+            this.prevBtn.Location = new System.Drawing.Point(343, 280);
+            this.prevBtn.Margin = new System.Windows.Forms.Padding(0, 0, 3, 0);
+            this.prevBtn.Name = "prevBtn";
+            this.prevBtn.Size = new System.Drawing.Size(137, 37);
+            this.prevBtn.TabIndex = 12;
+            this.prevBtn.Text = "< Previous Step";
+            this.prevBtn.UseVisualStyleBackColor = true;
+            this.prevBtn.Visible = false;
+            this.prevBtn.Click += new System.EventHandler(this.prevBtn_Click);
+            // 
+            // label4
+            // 
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(3, 3);
+            this.label4.Margin = new System.Windows.Forms.Padding(0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(600, 92);
+            this.label4.TabIndex = 9;
+            this.label4.Text = resources.GetString("label4.Text");
+            // 
+            // headingLabel
+            // 
+            this.headingLabel.Font = new System.Drawing.Font("Consolas", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.headingLabel.Location = new System.Drawing.Point(9, 9);
+            this.headingLabel.Margin = new System.Windows.Forms.Padding(0);
+            this.headingLabel.Name = "headingLabel";
+            this.headingLabel.Size = new System.Drawing.Size(614, 43);
+            this.headingLabel.TabIndex = 13;
+            this.headingLabel.Text = "LegendTV Setup - Name of Page";
+            // 
+            // tabPage4CalibrateGamma
+            // 
+            this.tabPage4CalibrateGamma.Location = new System.Drawing.Point(4, 5);
+            this.tabPage4CalibrateGamma.Name = "tabPage4CalibrateGamma";
+            this.tabPage4CalibrateGamma.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4CalibrateGamma.Size = new System.Drawing.Size(606, 219);
+            this.tabPage4CalibrateGamma.TabIndex = 3;
+            this.tabPage4CalibrateGamma.Text = "Gamma";
+            this.tabPage4CalibrateGamma.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5TunerDetection
+            // 
+            this.tabPage5TunerDetection.Location = new System.Drawing.Point(4, 5);
+            this.tabPage5TunerDetection.Name = "tabPage5TunerDetection";
+            this.tabPage5TunerDetection.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage5TunerDetection.Size = new System.Drawing.Size(606, 219);
+            this.tabPage5TunerDetection.TabIndex = 4;
+            this.tabPage5TunerDetection.Text = "tabPage5TunerDetection";
+            this.tabPage5TunerDetection.UseVisualStyleBackColor = true;
+            // 
+            // tabPage6TunerSetup
+            // 
+            this.tabPage6TunerSetup.Location = new System.Drawing.Point(4, 5);
+            this.tabPage6TunerSetup.Name = "tabPage6TunerSetup";
+            this.tabPage6TunerSetup.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage6TunerSetup.Size = new System.Drawing.Size(606, 219);
+            this.tabPage6TunerSetup.TabIndex = 5;
+            this.tabPage6TunerSetup.Text = "tabPage3";
+            this.tabPage6TunerSetup.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(338, 121);
+            this.label1.TabIndex = 10;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // SetupWizard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 326);
+            this.Controls.Add(this.headingLabel);
+            this.Controls.Add(this.prevBtn);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.nextBtn);
+            this.Name = "SetupWizard";
+            this.Text = "LegendTV";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1DisplayDevice.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tabPage2CalibrateBrightness.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button nextBtn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1DisplayDevice;
+        private System.Windows.Forms.TabPage tabPage2CalibrateBrightness;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button prevBtn;
+        private System.Windows.Forms.TabPage tabPage3CalibrateContrast;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label headingLabel;
+        private System.Windows.Forms.TabPage tabPage4CalibrateGamma;
+        private System.Windows.Forms.TabPage tabPage5TunerDetection;
+        private System.Windows.Forms.TabPage tabPage6TunerSetup;
+        private System.Windows.Forms.Label label1;
+
+    }
+}
+
---
+++ SE444/High-fidelity Models/LegendTV/TVMenu.Designer.cs
@@ -0,0 +1,150 @@
+namespace LegendTV
+{
+    partial class TVMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tvButton = new System.Windows.Forms.Button();
+            this.videoButton = new System.Windows.Forms.Button();
+            this.settingsButton = new System.Windows.Forms.Button();
+            this.musicButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tvButton
+            // 
+            this.tvButton.Location = new System.Drawing.Point(236, 55);
+            this.tvButton.Name = "tvButton";
+            this.tvButton.Size = new System.Drawing.Size(160, 70);
+            this.tvButton.TabIndex = 0;
+            this.tvButton.Text = "Watch Live TV";
+            this.tvButton.UseVisualStyleBackColor = true;
+            // 
+            // videoButton
+            // 
+            this.videoButton.Location = new System.Drawing.Point(402, 131);
+            this.videoButton.Name = "videoButton";
+            this.videoButton.Size = new System.Drawing.Size(160, 70);
+            this.videoButton.TabIndex = 1;
+            this.videoButton.Text = "Browse Recorded Programs";
+            this.videoButton.UseVisualStyleBackColor = true;
+            // 
+            // settingsButton
+            // 
+            this.settingsButton.Location = new System.Drawing.Point(236, 207);
+            this.settingsButton.Name = "settingsButton";
+            this.settingsButton.Size = new System.Drawing.Size(160, 70);
+            this.settingsButton.TabIndex = 2;
+            this.settingsButton.Text = "Change TV Settings";
+            this.settingsButton.UseVisualStyleBackColor = true;
+            // 
+            // musicButton
+            // 
+            this.musicButton.Location = new System.Drawing.Point(70, 131);
+            this.musicButton.Name = "musicButton";
+            this.musicButton.Size = new System.Drawing.Size(160, 70);
+            this.musicButton.TabIndex = 3;
+            this.musicButton.Text = "View TV Guide";
+            this.musicButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Consolas", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(614, 43);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "LegendTV";
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(9, 280);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(0);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(137, 37);
+            this.exitButton.TabIndex = 5;
+            this.exitButton.Text = "Back to Main Menu";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(149, 280);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(471, 37);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Now recording \"Family Guy\" on FOX";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(257, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 24);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "TV Menu";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TVMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 326);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.musicButton);
+            this.Controls.Add(this.settingsButton);
+            this.Controls.Add(this.videoButton);
+            this.Controls.Add(this.tvButton);
+            this.Name = "TVMenu";
+            this.Text = "LegendTV";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button tvButton;
+        private System.Windows.Forms.Button videoButton;
+        private System.Windows.Forms.Button settingsButton;
+        private System.Windows.Forms.Button musicButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+
+    }
+}
+
