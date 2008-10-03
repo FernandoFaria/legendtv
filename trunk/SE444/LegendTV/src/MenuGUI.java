e410cd54e20b9ca259b43f9bac1d4e2f21d7e1e9
bryan.eldridge <bryan.eldridge@fc7fc1dd-9c2b-0410-9c68-c9cb4ac4db1e>
2008-10-03 20:52:03 +0000

    Initial revision.  Stub; no functionality.

---
+++ trunk/SE444/LegendTV/src/MenuGUI.java
@@ -0,0 +1,20 @@
+/**
+ * MenuGUI.java
+ * 
+ * The "cross" form used for the major menus throughout the application.
+ * 
+ * $Id$
+ * 
+ * @author bje2515
+ *
+ */
+public class MenuGUI {
+
+	/**
+	 * For testing.
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
