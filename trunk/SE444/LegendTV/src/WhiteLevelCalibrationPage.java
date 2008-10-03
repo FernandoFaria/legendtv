21d1e3f909209573976617a66aab489ce1f75ed9
guy.paddock <guy.paddock@fc7fc1dd-9c2b-0410-9c68-c9cb4ac4db1e>
2008-10-03 22:56:14 +0000

    Crude wizard implementation completed.

---
+++ trunk/SE444/LegendTV/src/WhiteLevelCalibrationPage.java
@@ -0,0 +1,20 @@
+
+
+@SuppressWarnings("serial")
+public class WhiteLevelCalibrationPage extends WizardPage
+{
+	@Override
+	public WizardPage getNextPage()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTitle()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
