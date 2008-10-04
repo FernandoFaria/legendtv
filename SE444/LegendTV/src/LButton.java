e9f75cde3281211f9cca4848d368d828b46493c2
andrew.j.bona <andrew.j.bona@fc7fc1dd-9c2b-0410-9c68-c9cb4ac4db1e>
2008-10-04 00:51:23 +0000

    LButton intial commit

---
+++ SE444/LegendTV/src/LButton.java
@@ -0,0 +1,39 @@
+import java.awt.Color;
+import java.awt.Insets;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+
+/*
+ * LButton.java
+ *
+ * Version:
+ *	 $Id$
+ *
+ * Revisions:
+ *   $Log$
+ */
+public class LButton extends JButton{
+	
+	private static Icon buttonOffIcon = new ImageIcon("images/button_off_short.png");
+	private static Icon buttonOnIcon = new ImageIcon("images/button_on_short.png");
+	private Insets in = new Insets(0,0,0,0);
+	
+	public LButton(String text ){
+		super(text, buttonOffIcon );
+	
+		this.setBackground(Color.BLACK);
+		this.setIconTextGap(0);
+		this.setForeground(Color.WHITE);
+		this.setMargin(in);
+		this.setVerticalTextPosition(SwingConstants.CENTER);
+		this.setHorizontalTextPosition(SwingConstants.CENTER);
+		this.setBorderPainted(false);
+		this.setRolloverIcon(buttonOnIcon);
+	}
+
+	
+	
+}
