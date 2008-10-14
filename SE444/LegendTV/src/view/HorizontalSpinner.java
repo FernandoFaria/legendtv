7bde1d032268e29897184626136d39c7dc35c96f
andrew.j.bona <andrew.j.bona@fc7fc1dd-9c2b-0410-9c68-c9cb4ac4db1e>
2008-10-14 02:18:35 +0000

    removed Focus painted

--- SE444/LegendTV/src/view/HorizontalSpinner.java
+++ SE444/LegendTV/src/view/HorizontalSpinner.java
@@ -1,8 +1,11 @@
 package view;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
@@ -15,7 +18,7 @@ import javax.swing.*;
  * Revisions:
  *   $Log$
  */
-public class HorizontalSpinner extends JPanel {
+public class HorizontalSpinner extends JPanel implements ActionListener{
 	
 	private JButton leftButton, rightButton;
 	private Icon leftImage, rightImage;
@@ -34,15 +37,26 @@ public class HorizontalSpinner extends JPanel {
 		leftButton = new JButton(leftImage);
 		leftButton.setBackground(Color.DARK_GRAY);
 		leftButton.setBorderPainted(false);
+		leftButton.setActionCommand("Left");
+		leftButton.setFocusPainted(false);
+		leftButton.addActionListener(this);
 		
 		rightButton = new JButton(rightImage);
 		rightButton.setBackground(Color.DARK_GRAY);
 		rightButton.setBorderPainted(false);
+		rightButton.setActionCommand("Right");
+		rightButton.addActionListener(this);
+		rightButton.setFocusPainted(false);
 		
 		index = 0;
 		displayLabel = new JLabel(options[index]);
 		displayLabel.setForeground(Color.WHITE);
-		
+		int maxSize = 0;
+		for(int i = 1; i<optionChoices.length; i++){
+			if( optionChoices[maxSize].length() < optionChoices[i].length() ){
+				maxSize = i;
+			}
+		}		
 
 		this.setLayout( new BoxLayout(this, BoxLayout.X_AXIS) );		
 		this.add(leftButton);
@@ -50,7 +64,6 @@ public class HorizontalSpinner extends JPanel {
 		this.add(displayLabel);
 		this.add(Box.createHorizontalGlue());
 		this.add(rightButton);
-		
 		this.setBackground(Color.DARK_GRAY);
 		this.setVisible(true);
 	}
@@ -72,6 +85,16 @@ public class HorizontalSpinner extends JPanel {
 		}else{
 			index = (options.length-1);
 			displayLabel.setText( options[index] );
+		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getActionCommand().equals("Left")){
+			displayLeftOption();
+		}else if(e.getActionCommand().equals("Right")){
+			displayRightOption();
 		}
 	}
 }
