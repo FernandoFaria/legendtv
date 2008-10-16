dadf414af91130a156d5603ba57b4b7d5caed145
andrew.j.bona <andrew.j.bona@fc7fc1dd-9c2b-0410-9c68-c9cb4ac4db1e>
2008-10-16 02:07:02 +0000

    Added more buttons to button panel. Delete is properly functioning

--- trunk/SE444/LegendTV/src/view/RecordedProgramsGUI.java
+++ trunk/SE444/LegendTV/src/view/RecordedProgramsGUI.java
@@ -1,6 +1,8 @@
 package view;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Iterator;
 import java.util.LinkedList;
 
@@ -24,14 +26,14 @@ import view.Program;
  * Revisions:
  *   $Log$
  */
-public class RecordedProgramsGUI {
+public class RecordedProgramsGUI extends JPanel implements ActionListener{
 	
 	//GUI Components for the Frame and Panels
 	JFrame frame = new JFrame();
 	JPanel mainPanel;
 	
 	//GUI Components for the Button Bar
-	JButton backButton, selectButton, deleteButton;
+	JButton backButton, watchButton, deleteButton, optionsButton;
 	
 	//GUI Component for the description
 	JTextArea programDescription;
@@ -43,38 +45,40 @@ public class RecordedProgramsGUI {
 	private LinkedList<RecordedProgram> programs;
 	private JScrollPane tableScroller;	
 	private JTable recordedProgramsTable;
+	private Object[][] tableData;
+	private DefaultTableModel tableModel;
 	private JTableHeader tableHead;
 	
 	public RecordedProgramsGUI(LinkedList<RecordedProgram> recordedPrograms){
 		programs = recordedPrograms;
 		//programListModel = new DefaultListModel();
 		
-		Object[][] tableData = new Object[ programs.size() ][2];
+		tableData = new Object[ programs.size() ][2];
 				
 		//Iterate through the LinkedList and add the elements to the ListModel
 		Iterator<RecordedProgram> it = programs.iterator();
 		int i = 0;
 		while(it.hasNext()){
 			RecordedProgram temp = (RecordedProgram) it.next();
-			tableData[i][0] = temp.getTitle();
+			tableData[i][0] = temp;
 			tableData[i][1] = temp.getRecordedDate();
 			i++;
 		}
 		
 		//Set up the specifications for the Table
-		recordedProgramsTable = new JTable(new DefaultTableModel(tableData, new Object[]{"Title", "Recorded Date"})) {
-		    public boolean isCellEditable(int row, int col) { return false; }
-		};
+		tableModel = new DefaultTableModel(tableData, new Object[]{"Title", "Recorded Date"}) {
+		    public boolean isCellEditable(int row, int col) { return false; }};
+		recordedProgramsTable = new JTable(tableModel);
 		recordedProgramsTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		recordedProgramsTable.setShowVerticalLines(false);
 		recordedProgramsTable.setShowHorizontalLines(false);
 		recordedProgramsTable.setBackground(Color.WHITE);
 		recordedProgramsTable.setAutoCreateRowSorter(true);
+		recordedProgramsTable.changeSelection(0, 0, false, false);
 		tableHead = recordedProgramsTable.getTableHeader();  //The specifications for the column headers
 		tableHead.setReorderingAllowed(false);
 		tableScroller = new JScrollPane(recordedProgramsTable);  //The scroll bar for the table
-		//tableScroller.setPreferredSize(new Dimension(1000, 500) );
-		tableScroller.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);	
+		tableScroller.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 				
 		//Set up the Label
 		screenDescription = new JLabel("Recorded Programs");
@@ -83,7 +87,7 @@ public class RecordedProgramsGUI {
 		screenDescription.setFont(labelFont);
 		
 		//Set up the program description 
-		String displayString = programs.get(0).toString();		
+		String displayString = programs.get(0).toFullDescription();		
 		programDescription = new JTextArea();
 		programDescription.setBackground(Color.DARK_GRAY);
 		programDescription.setForeground(Color.WHITE);
@@ -91,9 +95,19 @@ public class RecordedProgramsGUI {
 		programDescription.setEditable(false);
 		
 		//Set up the Button(s)
-		backButton = new LButton("Go Back");
-		selectButton = new LButton("Select");
+		backButton = new LButton("Back to TV Menu");
+		backButton.setActionCommand("Back");
+		backButton.addActionListener(this);
+		watchButton = new LButton("Watch");
+		watchButton.setActionCommand("Watch");
+		watchButton.addActionListener(this);
 		deleteButton = new LButton("Delete");
+		deleteButton.setActionCommand("Delete");
+		deleteButton.addActionListener(this);
+		optionsButton = new LButton("Program Options");
+		optionsButton.setActionCommand("Options");
+		optionsButton.addActionListener(this);
+		
 		
 		//Set up the panels and Boxes
 		JPanel programBox = new JPanel(new GridLayout(2,1));
@@ -107,7 +121,6 @@ public class RecordedProgramsGUI {
 		mainPanel.add(screenDescription);
 		mainPanel.add(Box.createVerticalStrut(25));
 		mainPanel.add(componentBox);
-		//mainPanel.add(Box.createGlue());
 		mainPanel.setBackground(Color.BLACK);	
 				
 		componentBox.add(programBox);
@@ -123,11 +136,12 @@ public class RecordedProgramsGUI {
 		tableScroller.setBackground(Color.BLACK);
 		programBox.setBackground(Color.BLACK);
 		
+		buttonsBox.add(watchButton);
 		buttonsBox.add(deleteButton);
-		buttonsBox.add(selectButton);
-		Component vertComp = Box.createVerticalStrut(50);
-		vertComp.setMaximumSize( new Dimension(50, 0) );
-		//buttonsBox.add();
+		buttonsBox.add(optionsButton);
+		Component c = Box.createVerticalStrut(35);
+		c.setMaximumSize(new Dimension(0,35));
+		buttonsBox.add(c);
 		buttonsBox.add(backButton);		
 			
 		//Frame Settings 
@@ -135,26 +149,51 @@ public class RecordedProgramsGUI {
 		frame.setTitle("LegendTV - Recorded Programs");
 		frame.pack();
 		frame.setVisible(true);
+		
+		//Grant Focus to the first thing in the table
+		recordedProgramsTable.requestFocusInWindow();
 	}
 	
 	public static void main(String[] args){
 		LinkedList<RecordedProgram> list = new LinkedList<RecordedProgram>();
 		list.add( new RecordedProgram("Family Guy", "Peter does something stupid!", Program.TV_14, 30, "10/10/2008", "10/30/2008") );
-		list.add( new RecordedProgram("Family Guy", "Peter does something stupid!", Program.TV_14, 30, "10/10/2008", "10/30/2008") );
-		list.add( new RecordedProgram("Price is Right!", "Peter does something stupid!", Program.TV_14, 30, "10/10/2008", "10/30/2008") );
-		list.add( new RecordedProgram("CSI: NY", "Peter does something stupid!", Program.TV_14, 30, "10/11/2008", "10/30/2008") );
-		list.add( new RecordedProgram("CSI: RIT", "Peter does something stupid!", Program.TV_14, 30, "10/12/2008", "10/30/2008") );
-		list.add( new RecordedProgram("Altos", "Peter does something stupid!", Program.TV_14, 30, "10/08/2008", "10/30/2008") );
-		list.add( new RecordedProgram("Altos", "Peter does something stupid!", Program.TV_14, 30, "10/15/2008", "10/30/2008") );
-		list.add( new RecordedProgram("Altos", "Peter does something stupid!", Program.TV_14, 30, "10/22/2008", "10/30/2008") );
-		list.add( new RecordedProgram("Altos", "Peter does something stupid!", Program.TV_14, 30, "10/10/2008", "10/30/2008") );
-		list.add( new RecordedProgram("Altos", "Peter does something stupid!", Program.TV_14, 30, "10/10/2008", "10/30/2008") );
-		list.add( new RecordedProgram("Altos", "Peter does something stupid!", Program.TV_14, 30, "10/10/2008", "10/30/2008") );
-		list.add( new RecordedProgram("Altos", "Peter does something stupid!", Program.TV_14, 30, "10/10/2008", "10/30/2008") );
-		list.add( new RecordedProgram("Altos", "Peter does something stupid!", Program.TV_14, 30, "10/10/2008", "10/30/2008") );
+		list.add( new RecordedProgram("Family Guy", "Peter does something stupid Again!", Program.TV_MA, 30, "10/10/2008", "10/30/2008") );
+		list.add( new RecordedProgram("Price is Right!", "We miss Bob Barker...", Program.TV_14, 30, "10/10/2008", "10/30/2008") );
+		list.add( new RecordedProgram("CSI: NY", "Every cast member is gone...", Program.TV_14, 30, "10/11/2008", "10/30/2008") );
+		list.add( new RecordedProgram("CSI: RIT", "Someone has stolen the tiger!", Program.TV_14, 30, "10/12/2008", "10/30/2008") );
+		list.add( new RecordedProgram("Altos", "There is drama in Altoville", Program.TV_14, 30, "10/08/2008", "10/30/2008") );
+		list.add( new RecordedProgram("CSI: Miami", "It looks like bird.... is the word...", Program.TV_14, 30, "10/15/2008", "10/30/2008") );
+		list.add( new RecordedProgram("CSI: Miami", "Why did you tape this show?", Program.TV_14, 30, "10/22/2008", "10/30/2008") );
+		list.add( new RecordedProgram("Altos", "Peter does something stupid! And he's not even in this show!", Program.TV_G, 30, "10/10/2008", "10/30/2008") );
+		list.add( new RecordedProgram("Altos", "The Sopranos are getting jealous...", Program.TV_14, 30, "10/12/2008", "10/30/2008") );
+		list.add( new RecordedProgram("Altos", "Altos make perfect harmony with the Tenors", Program.TV_14, 30, "10/14/2008", "10/30/2008") );
+		list.add( new RecordedProgram("Altos", "I'm running out of ideas...", Program.TV_14, 30, "10/16/2008", "10/30/2008") );
+		list.add( new RecordedProgram("Altos", "And I'm done!", Program.TV_14, 30, "10/18/2008", "10/30/2008") );
 
 		
 		RecordedProgramsGUI a = new RecordedProgramsGUI(list);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		String command = e.getActionCommand();
+		
+		if(command.equals("Delete")){
+			
+			int selectedRow = recordedProgramsTable.getSelectedRow();
+			Object o = recordedProgramsTable.getValueAt(selectedRow, 0);
+			programs.remove(o);
+			tableModel.removeRow(selectedRow);
+			
+		}else if(command.equals("Watch")){
+			
+		}else if(command.equals("Options")){
+			
+		}else if(command.equals("Back")){
+			
+		}
+		
 	}
 
 }
