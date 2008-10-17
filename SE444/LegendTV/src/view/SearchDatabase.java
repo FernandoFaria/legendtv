f5dc2748f7afaa366e39cef84cf467014d96a6f2
andrew.j.bona <andrew.j.bona@fc7fc1dd-9c2b-0410-9c68-c9cb4ac4db1e>
2008-10-17 20:46:47 +0000

    Searching works

--- SE444/LegendTV/src/view/SearchDatabase.java
+++ SE444/LegendTV/src/view/SearchDatabase.java
@@ -16,12 +16,16 @@ public class SearchDatabase {
 	
 	private LinkedList <ProgramListing>showTitlesList;
 	private LinkedList <ProgramListing>showTimesList;
+	private LinkedList <ProgramListing>shows;
 	
 	public SearchDatabase(){
 		showTitlesList = new LinkedList<ProgramListing>();
+		showTimesList = new LinkedList<ProgramListing>();
+		shows = new LinkedList<ProgramListing>();
 		Channel cbs = new Channel((short)4, "CBS");
-		showTimesList.add( new ProgramListing("CSI" , "The original", Program.TV_14, 30, cbs , "10/16/08", "9:00PM") );
-		showTimesList.add( new ProgramListing("CSI: Miami", "It looks like that bird... is the word..", Program.TV_14, 29, cbs, "10/18/08", "10:00PM" ) );
+		shows.add( new ProgramListing("CSI" , "The original", Program.TV_14, 30, cbs , "10/16/08", "9:00PM") );
+		shows.add( new ProgramListing("CSI" , "The original times two", Program.TV_14, 30, cbs , "10/23/08", "9:00PM"));
+		shows.add( new ProgramListing("CSI: Miami", "It looks like that bird... is the word..", Program.TV_14, 29, cbs, "10/18/08", "10:00PM" ) );
 		//showTitlesList.add("CSI: RIT");
 		//showTitlesList.add("CSI: Wasilla");
 		
@@ -35,24 +39,45 @@ public class SearchDatabase {
 	}
 	
 	public void getShowTitles(DefaultListModel titles, String searchKey){
-
-		Iterator<ProgramListing> it = showTitlesList.iterator();
+		titles.clear();
+		Iterator<ProgramListing> it = shows.iterator();
 		int i = 0;
+		TreeSet<String> programTitles = new TreeSet<String>();
 		while( it.hasNext() ){
-			titles.add(i, it.next());
+			ProgramListing p = it.next();
+			if( p.getTitle().contains(searchKey) ){
+				programTitles.add( p.getTitle() );
+			}
+			showTimesList.add( p );
+		}
+			
+		Iterator<String> itString = programTitles.iterator();
+
+		while( itString.hasNext() ){
+			titles.add( i, itString.next() );
 			i++;
+		}
+		if( titles.isEmpty() ){	
+			titles.add(0, "No shows found");
 		}
 	}
 	
 	public void getTimesList( DefaultListModel times, String searchKey ){
-		
-		Iterator<ProgramListing> it = showTimesList.iterator();
-		int i = 0;
-		while( it.hasNext() ){
-			times.add(i, it.next());
-			i++;
-		}		
-		
+		times.clear();
+		if( !searchKey.equals("No shows found") ){
+			Iterator<ProgramListing> it = showTimesList.iterator();
+			int i = 0;
+			while( it.hasNext() ){
+				ProgramListing p = it.next();
+				System.out.println( p.getTitle() );
+				if(p.getTitle().equals(searchKey)){
+					times.add(i, p);
+					i++;
+				}
+			}
+			
+		}
 	}
+	
 
 }
