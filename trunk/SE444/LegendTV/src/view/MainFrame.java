02866d9cd9a70153cd63d02be62460623e20a57a
elutley <elutley@fc7fc1dd-9c2b-0410-9c68-c9cb4ac4db1e>
2008-10-18 01:19:37 +0000

    MainFrame now launches Main Menu, which can in turn display TV menu (although TV menu is incorrectly named Main Menu).

--- trunk/SE444/LegendTV/src/view/MainFrame.java
+++ trunk/SE444/LegendTV/src/view/MainFrame.java
@@ -3,8 +3,16 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import view.controls.SVGButton;
+import view.utils.ScreenManager;
+import view.utils.UIHelper;
 
 /**
  * Container window for screens in the LegendTV Swing-based user interface.
@@ -14,6 +22,8 @@ import javax.swing.JFrame;
 @SuppressWarnings("serial")
 public class MainFrame extends JFrame
 {
+	private final ScreenManager screenManager;
+
 	/**
 	 * Constructor for MainFrame.
 	 * 
@@ -24,6 +34,107 @@ public class MainFrame extends JFrame
 		super("LegendTV");
 		
 		this.setupControls();
+		
+		screenManager = new ScreenManager( this );
+		MenuGUI tvMenu = createTVMenu();
+		MenuGUI mainMenu = createMainMenu( tvMenu );
+		screenManager.show( mainMenu );
+	}
+	
+	private MenuGUI createMainMenu( MenuGUI tvMenu ) {
+		final MenuGUI finalTvMenu = tvMenu;
+		SVGButton tv = UIHelper.createButton( "TV", 
+				new ActionListener() {
+				@Override
+				public void actionPerformed( ActionEvent e ) {
+					screenManager.show( finalTvMenu );
+				}
+			} );
+		
+		SVGButton settings = UIHelper.createButton( "Settings", 
+				new ActionListener() {
+				@Override
+				public void actionPerformed( ActionEvent e ) {
+					JOptionPane.showMessageDialog( MainFrame.this, 
+					"This feature is not yet implemented" );
+				}
+			} );
+			
+			SVGButton music = UIHelper.createButton( "Music", 
+					new ActionListener() {
+					@Override
+					public void actionPerformed( ActionEvent e ) {
+						JOptionPane.showMessageDialog( MainFrame.this, 
+						"This feature is not yet implemented" );
+					}
+				} );
+			
+			SVGButton video = UIHelper.createButton( "Video", 
+					new ActionListener() {
+					@Override
+					public void actionPerformed( ActionEvent e ) {
+						JOptionPane.showMessageDialog( MainFrame.this, 
+								"This feature is not yet implemented" );
+					}
+				} );
+		return new MenuGUI(tv, music, video, settings, createExitButton() );
+	}
+
+	private MenuGUI createTVMenu() {
+		SVGButton search = UIHelper.createButton( "Search TV Listings", 
+			new ActionListener() {
+			@Override
+			public void actionPerformed( ActionEvent e ) {
+				JComponent screen = new SearchGuideGUI();
+				screenManager.show( screen );
+			}
+		} );
+		
+		SVGButton watchTv = UIHelper.createButton( "Watch Live TV", 
+				new ActionListener() {
+				@Override
+				public void actionPerformed( ActionEvent e ) {
+					JComponent screen = new PlaybackScreen();
+					screenManager.show( screen );
+				}
+			} );
+		
+		SVGButton browseListings = UIHelper.createButton( "View TV Guide", 
+				new ActionListener() {
+				@Override
+				public void actionPerformed( ActionEvent e ) {
+					JComponent screen = new GuideView();
+					screenManager.show( screen );
+				}
+			} );
+		
+		SVGButton browseRecordings = UIHelper.createButton( "Browse Recorded Programs", 
+				new ActionListener() {
+				@Override
+				public void actionPerformed( ActionEvent e ) {
+					JComponent screen = new RecordedProgramsGUI( null );
+					screenManager.show( screen );
+				}
+			} );
+		return new MenuGUI(watchTv, browseListings, browseRecordings, search, createBackButton() );
+	}
+	
+	private SVGButton createExitButton() {
+		return UIHelper.createButton( "Exit", new ActionListener() {
+			@Override
+			public void actionPerformed( ActionEvent e ) {
+				MainFrame.this.dispose();
+			}
+		} );
+	}
+	
+	private SVGButton createBackButton() {
+		return UIHelper.createButton( "Back", new ActionListener() {
+			@Override
+			public void actionPerformed( ActionEvent e ) {
+				screenManager.back();
+			}
+		} );
 	}
 	
 	/**
