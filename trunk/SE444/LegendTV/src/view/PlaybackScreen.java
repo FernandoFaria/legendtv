c0896b5e8a5de2935f59f062aacf12e91ca96be2
andrew.j.bona <andrew.j.bona@fc7fc1dd-9c2b-0410-9c68-c9cb4ac4db1e>
2008-10-18 02:14:07 +0000

    Bottom buttons change label

--- trunk/SE444/LegendTV/src/view/PlaybackScreen.java
+++ trunk/SE444/LegendTV/src/view/PlaybackScreen.java
@@ -10,8 +10,10 @@ import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingWorker;
 import javax.swing.event.MouseInputAdapter;
@@ -19,14 +21,18 @@ import javax.swing.event.MouseInputAdapter;
 import view.controls.SVGButton;
 import view.utils.UIHelper;
 
-public class PlaybackScreen extends JComponent
+public class PlaybackScreen extends JComponent implements ActionListener
 {
 	private static int HIDE_DELAY	= 5000;
 	
+	
 	private JPanel 		playbackPanel;
-	private SVGButton	playBtn;
+	//Buttons
+	//Play, Fast forward, skip forward, skip backward, rewind, stop
+	private JButton		playPauseBtn, skipForwardBtn, fastForwardBtn, skipBackwardBtn, rewindBtn, stopBtn;
 	private	Timer		hideTimer;
 	private TimerTask	hideTimerTask;
+	private JLabel		currentStatus;
 	
 	public PlaybackScreen()
 	{
@@ -50,25 +56,61 @@ public class PlaybackScreen extends JComponent
 	
 		this.playbackPanel	= new JPanel();
 
-		this.playbackPanel.setBackground(Color.WHITE);
+		this.playbackPanel.setBackground(Color.DARK_GRAY);
 		this.playbackPanel.setVisible(false);
 		this.playbackPanel.setPreferredSize(new Dimension(1, 80));
 		
 		this.add(this.playbackPanel, BorderLayout.SOUTH);
 		
-		this.playBtn	= UIHelper.createButton("Play", new ActionListener()
-		{
-			@Override
-			public void actionPerformed(ActionEvent e)
-			{
-				// TODO Auto-generated method stub	
-			}
-		});	
-
-		this.playBtn.setBackground(this.playbackPanel.getBackground());
-		this.playBtn.setPreferredSize(new Dimension(230, 76));
 		
-		this.playbackPanel.add(this.playBtn);
+		//Set the Buttons 
+		this.playPauseBtn = new JButton("||");
+		this.playPauseBtn.addActionListener(this);
+		this.playPauseBtn.setActionCommand("Play/Pause");
+		this.playPauseBtn.setBackground(Color.BLACK);
+		this.playPauseBtn.setForeground(Color.WHITE);
+		
+		this.skipForwardBtn = new JButton("|> |");
+		this.skipForwardBtn.addActionListener(this);
+		this.skipForwardBtn.setActionCommand("Skip Forward");
+		this.skipForwardBtn.setBackground(Color.BLACK);
+		this.skipForwardBtn.setForeground(Color.WHITE);
+		
+		this.fastForwardBtn = new JButton("|> |>");
+		this.fastForwardBtn.addActionListener(this);
+		this.fastForwardBtn.setActionCommand("Fast Forward");
+		this.fastForwardBtn.setBackground(Color.BLACK);
+		this.fastForwardBtn.setForeground(Color.WHITE);
+		
+		this.skipBackwardBtn = new JButton("| <|");
+		this.skipBackwardBtn.addActionListener(this);
+		this.skipBackwardBtn.setActionCommand("Skip Backward");
+		this.skipBackwardBtn.setBackground(Color.BLACK);
+		this.skipBackwardBtn.setForeground(Color.WHITE);
+		
+		this.rewindBtn = new JButton("<| <|");
+		this.rewindBtn.addActionListener(this);
+		this.rewindBtn.setActionCommand("Rewind");
+		this.rewindBtn.setBackground(Color.BLACK);
+		this.rewindBtn.setForeground(Color.WHITE);
+		
+		this.stopBtn = new JButton("Stop");
+		this.stopBtn.addActionListener(this);
+		this.stopBtn.setActionCommand("Stop");
+		this.stopBtn.setBackground(Color.BLACK);
+		this.stopBtn.setForeground(Color.WHITE);
+		
+		this.currentStatus = new JLabel("Playing");
+		this.add(this.currentStatus, BorderLayout.NORTH);
+
+		//this.playPauseBtn.setBackground(this.playbackPanel.getBackground());
+		//this.playPauseBtn.setPreferredSize(new Dimension(230, 76));
+		
+		this.playbackPanel.add(this.skipBackwardBtn);
+		this.playbackPanel.add(this.rewindBtn);
+		this.playbackPanel.add(this.playPauseBtn);
+		this.playbackPanel.add(this.fastForwardBtn);
+		this.playbackPanel.add(this.skipForwardBtn);
 	}
 	
 	protected void showPanel()
@@ -79,7 +121,6 @@ public class PlaybackScreen extends JComponent
 			this.hideTimerTask.cancel();
 		
 		// Show the panel.
-		System.out.println("Showing...");
 		this.playbackPanel.setVisible(true);
 		
 		// Create a new timer task to hide the panel after a period of time 
@@ -89,8 +130,6 @@ public class PlaybackScreen extends JComponent
 			{
 				SwingWorker<Boolean, Object>	worker;
 		
-				
-				System.out.println("Setting up for hide...");
 				
 				// Invoke the control update to the event dispatch thread.
 				worker = new SwingWorker<Boolean, Object>()
@@ -104,7 +143,6 @@ public class PlaybackScreen extends JComponent
 					@Override
 					protected void done()
 					{
-						System.out.println("Hiding...");
 						PlaybackScreen.this.playbackPanel.setVisible(false);
 					}
 				};
@@ -125,5 +163,23 @@ public class PlaybackScreen extends JComponent
 		main.add(new PlaybackScreen());
 		
 		main.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		String actionCommand = e.getActionCommand();
+		if(actionCommand.equals("Play/Pause")){
+			if(this.playPauseBtn.getText().equals("||")){
+				this.playPauseBtn.setText("|>");
+				currentStatus.setText("Paused");
+			}else{
+				this.playPauseBtn.setText("||");
+				currentStatus.setText("Playing");
+			}
+		}else{
+			currentStatus.setText(e.getActionCommand());
+			this.playPauseBtn.setText("|>");
+		}
 	}
 }
