0d32120ea646b869f2d20dd5dff793ed5b371d74
guy.paddock <guy.paddock@fc7fc1dd-9c2b-0410-9c68-c9cb4ac4db1e>
2008-10-23 23:22:25 +0000

    Fixed focus.

---
+++ SE444/LegendTV/src/view/utils/ListBasedFocusTraversalPolicy.java
@@ -0,0 +1,70 @@
+package view.utils;
+
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.FocusTraversalPolicy;
+import java.util.List;
+
+public class ListBasedFocusTraversalPolicy extends FocusTraversalPolicy
+{
+	private List<Component> focusPath;
+
+	public ListBasedFocusTraversalPolicy(List<Component> focusPath)
+	{
+		this.focusPath = focusPath;
+	}
+
+	@Override
+	public Component getFirstComponent(Container container)
+	{
+		return this.focusPath.get(0);
+	}
+
+	@Override
+	public Component getComponentAfter(Container container,
+			Component component)
+	{
+		int				curIndex;
+		Component		retComponent	= null;
+		
+		curIndex		= this.focusPath.indexOf(component);
+		
+		if (curIndex != -1)
+		{
+			if (curIndex == (this.focusPath.size() - 1))
+				retComponent	= this.getFirstComponent(container);
+			
+			else
+				retComponent	= this.focusPath.get(curIndex + 1);
+		}
+		
+		return retComponent;
+	}
+
+	@Override
+	public Component getComponentBefore(Container container,
+			Component component)
+	{
+		int				curIndex;
+		Component		retComponent	= null;
+		
+		curIndex		= this.focusPath.indexOf(component);
+		
+		if ((curIndex != -1) && (curIndex > 0))
+			retComponent	= this.focusPath.get(curIndex - 1);
+		
+		return retComponent;
+	}
+
+	@Override
+	public Component getDefaultComponent(Container container)
+	{
+		return this.getFirstComponent(container);
+	}
+
+	@Override
+	public Component getLastComponent(Container container)
+	{
+		return this.focusPath.get(this.focusPath.size() - 1);
+	}
+}
