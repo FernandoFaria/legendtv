ee985503759722b5fe42a3eccea0e5ca16cd1a36
bryan.eldridge <bryan.eldridge@fc7fc1dd-9c2b-0410-9c68-c9cb4ac4db1e>
2008-10-23 17:55:47 +0000

    Now responds to use of keyboard arrow and enter keys.

--- trunk/SE444/LegendTV/src/view/MenuGUI.java
+++ trunk/SE444/LegendTV/src/view/MenuGUI.java
@@ -1,6 +1,9 @@
 package view;
 import javax.swing.*;
+
 import java.awt.*;
+import java.awt.event.*;
+
 import javax.swing.plaf.basic.*;
 
 import view.utils.UIHelper;
@@ -18,16 +21,26 @@ import view.controls.SVGButton;
  */
 public class MenuGUI extends JPanel {
 	
+	private SVGButton northButton;
+	private SVGButton westButton;
+	private SVGButton eastButton;
+	private SVGButton southButton;
+	private SVGButton bottomButton;
+	
 	public MenuGUI(String title, SVGButton northButton, SVGButton westButton,
 			SVGButton eastButton, SVGButton southButton,
 			SVGButton bottomButton) {
+		
+		this.northButton = northButton;
+		this.westButton = westButton;
+		this.eastButton = eastButton;
+		this.southButton = southButton;
+		this.bottomButton = bottomButton;
 		
 		GridBagConstraints gbc = new GridBagConstraints();
 		
     	JLabel titleLabel = new JLabel("LegendTV");
     	JLabel centerLabel = new JLabel(title, SwingConstants.CENTER);
-    	
-
     	
     	titleLabel.setFont(UIHelper.getHeadingFont());
     	titleLabel.setForeground(UIHelper.getForegroundColor());
@@ -92,7 +105,7 @@ public class MenuGUI extends JPanel {
     	gbc.gridheight = 3;
     	gbc.weightx = 0.04;
     	gbc.weighty = 0.03;
-    	this.add(northButton, gbc);
+    	this.add(this.northButton, gbc);
     	
     	gbc.gridx = 10;
     	gbc.gridy = 2;
@@ -124,7 +137,7 @@ public class MenuGUI extends JPanel {
     	gbc.gridheight = 3;
     	gbc.weightx = 0.04;
     	gbc.weighty = 0.03;
-    	this.add(westButton, gbc);
+    	this.add(this.westButton, gbc);
     	
     	gbc.gridx = 5;
     	gbc.gridy = 6;
@@ -156,7 +169,7 @@ public class MenuGUI extends JPanel {
     	gbc.gridheight = 3;
     	gbc.weightx = 0.04;
     	gbc.weighty = 0.03;
-    	this.add(eastButton, gbc);
+    	this.add(this.eastButton, gbc);
     	
     	gbc.gridx = 15;
     	gbc.gridy = 6;
@@ -188,7 +201,7 @@ public class MenuGUI extends JPanel {
     	gbc.gridheight = 3;
     	gbc.weightx = 0.04;
     	gbc.weighty = 0.03;
-    	this.add(southButton, gbc);
+    	this.add(this.southButton, gbc);
     	
     	gbc.gridx = 10;
     	gbc.gridy = 10;
@@ -220,7 +233,7 @@ public class MenuGUI extends JPanel {
     	gbc.gridheight = 1;
     	gbc.weightx = 0.02;
     	gbc.weighty = 0.01;
-    	this.add(bottomButton, gbc);
+    	this.add(this.bottomButton, gbc);
     	
     	gbc.gridx = 9;
     	gbc.gridy = 14;
@@ -238,8 +251,64 @@ public class MenuGUI extends JPanel {
     	gbc.weighty = 0.01;
     	this.add(Box.createGlue(), gbc);
     	
+    	this.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("UP"),
+    			"UPkeypressed");
+    	this.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("LEFT"),
+				"LEFTkeypressed");
+    	this.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("RIGHT"),
+				"RIGHTkeypressed");
+    	this.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("DOWN"),
+				"DOWNkeypressed");
+    	this.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("ENTER"),
+				"ENTERkeypressed");
+
+    	this.getActionMap().put("UPkeypressed", new MenuGUIAction("UP"));
+    	this.getActionMap().put("LEFTkeypressed", new MenuGUIAction("LEFT"));
+    	this.getActionMap().put("RIGHTkeypressed", new MenuGUIAction("RIGHT"));
+    	this.getActionMap().put("DOWNkeypressed", new MenuGUIAction("DOWN"));
+    	this.getActionMap().put("ENTERkeypressed", new MenuGUIAction("ENTER"));
 	}
 	
+	public SVGButton getNorthButton() {
+		return northButton;
+	}
+
+	public void setNorthButton(SVGButton northButton) {
+		this.northButton = northButton;
+	}
+
+	public SVGButton getWestButton() {
+		return westButton;
+	}
+
+	public void setWestButton(SVGButton westButton) {
+		this.westButton = westButton;
+	}
+
+	public SVGButton getEastButton() {
+		return eastButton;
+	}
+
+	public void setEastButton(SVGButton eastButton) {
+		this.eastButton = eastButton;
+	}
+
+	public SVGButton getSouthButton() {
+		return southButton;
+	}
+
+	public void setSouthButton(SVGButton southButton) {
+		this.southButton = southButton;
+	}
+
+	public SVGButton getBottomButton() {
+		return bottomButton;
+	}
+
+	public void setBottomButton(SVGButton bottomButton) {
+		this.bottomButton = bottomButton;
+	}
+
 	/**
 	 * For testing.
 	 */
@@ -271,5 +340,77 @@ public class MenuGUI extends JPanel {
 		mainFrame.setUndecorated(true);
 		mainFrame.setVisible(true);
 	}
+	
+	private class MenuGUIAction extends AbstractAction {
 
+		private String key;
+		
+		public MenuGUIAction(String key) {
+			this.key = key;
+		}
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (this.key.equals("UP")) {
+				getNorthButton().requestFocus();
+				unfocusAllButtons();
+				getNorthButton().setLooksFocused(true);
+			}
+			if (this.key.equals("LEFT")) {
+				getWestButton().requestFocus();
+				unfocusAllButtons();
+				getWestButton().setLooksFocused(true);
+			}
+			if (this.key.equals("RIGHT")) {
+				getEastButton().requestFocus();
+				unfocusAllButtons();
+				getEastButton().setLooksFocused(true);
+			}
+			if (this.key.equals("DOWN")) {
+				if (getSouthButton().isFocusOwner()) {
+					getBottomButton().requestFocus();
+					unfocusAllButtons();
+					getBottomButton().setLooksFocused(true);
+				}
+				else {
+					getSouthButton().requestFocus();
+					unfocusAllButtons();
+					getSouthButton().setLooksFocused(true);
+				}
+			}
+			if (this.key.equals("ENTER")) {
+				if (getNorthButton().isFocusOwner()) {
+					getNorthButton().doClick();
+					unfocusAllButtons();
+				}
+				if (getWestButton().isFocusOwner()) {
+					getWestButton().doClick();
+					unfocusAllButtons();
+				}
+				if (getEastButton().isFocusOwner()) {
+					getEastButton().doClick();
+					unfocusAllButtons();
+				}
+				if (getSouthButton().isFocusOwner()) {
+					getSouthButton().doClick();
+					unfocusAllButtons();
+				}
+				if (getBottomButton().isFocusOwner()) {
+					getBottomButton().doClick();
+					unfocusAllButtons();
+				}
+			}
+		}
+		
+		private void unfocusAllButtons() {
+			getNorthButton().setLooksFocused(false);
+			getWestButton().setLooksFocused(false);
+			getEastButton().setLooksFocused(false);
+			getSouthButton().setLooksFocused(false);
+			getBottomButton().setLooksFocused(false);
+		}
+	}
 }
+
+
+
